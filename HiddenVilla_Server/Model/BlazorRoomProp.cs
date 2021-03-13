7796eb7ce5288aca64ea7151021e8a73b510bd15
ronaldpaglinawan <ronaldpaglinawan@live.com.ph>
2021-03-13 10:18:01 +0800

    * Index.razor: * BlazorRoom.cs: * BlazorRoomProp.cs:
    * HiddenVilla_Server.csproj: added BlazorRoomProp.cs

---
+++ HiddenVilla_Server/Model/BlazorRoomProp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
