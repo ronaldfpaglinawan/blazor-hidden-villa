97afcc69a7fc297925628d01a18ca7002ef83a18
ronaldpaglinawan <ronaldpaglinawan@live.com.ph>
2021-05-22 15:27:48 +0800

    added new projects and added ApplicationDbContext

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+        }
+    }
+}
